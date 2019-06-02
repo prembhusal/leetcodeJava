d88fa6dc37a4b7fe18f45c4d2933d718bb3bb833
prembhusal <remp.bhusal@gmail.com>
2019-06-01 20:30:34 -0400

    Update findOddCount.java

--- src/practice/findOddCount.java
+++ src/practice/findOddCount.java
@@ -1,1 +1,24 @@
-
+//from the given array of numbers find the numbers which has odd number of repetition
+public findOddCount{
+ public static void main(String [] args){
+  
+   int [] arr = {3,2,3,4,5,4,6,9,3};
+    HashMap<Integer,Integer>  objMap = new HashMap<Integer,Integer>();
+    for (Integer entry: arr){
+        if ( objMap.get(entry)!= null){
+            objMap.put(entry,objMap.get(entry) +1 );
+        }else{
+            objMap.put(entry,1);
+        }
+    }
+    List<Integer> lis = new ArrayList<Integer>();
+    for ( Integer key : objMap.keySet()){
+        
+        if( objMap.get(key) %2 != 0){
+        System.out.println("key: "+key);
+        System.out.println("value: "+objMap.get(key));
+        }
+    } 
+ }
+  
+}
